22347c5ea0722fb1f36524064ceedbb4540f9721
Nathaneil Ward <nathaneilw@gmail.com>
2017-08-01 14:20:21 +0200

    got through a crapton of errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+
+  get "tasks", to: "tasks#index"
+
+  get "tasks/new", to: "tasks#new"
+  delete "tasks/:id", to: "tasks#destroy", as: :task
+
+  get "tasks/:id", to: "tasks#show"
+
+  post "tasks", to: "tasks#create"
+
+  get "tasks/:id/edit", to: "tasks#edit"
+
+  patch "tasks/:id", to: "tasks#update"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
